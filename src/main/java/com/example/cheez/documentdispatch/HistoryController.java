2886b552e89c0c7afce112124fbfbf6a7480d2e0
Abdullah <95099545+abdullah-9211@users.noreply.github.com>
2023-05-03 23:50:27 +0500

    History Page made

--- src/main/java/com/example/cheez/documentdispatch/HistoryController.java
+++ src/main/java/com/example/cheez/documentdispatch/HistoryController.java
@@ -1,4 +1,21 @@
 package com.example.cheez.documentdispatch;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
 public class HistoryController {
+
+    @FXML
+    private Button backbtn;
+
+    @FXML
+    private TableView<?> historyTable;
+
+    @FXML
+    void goBack(ActionEvent event) {
+
+    }
+
 }
