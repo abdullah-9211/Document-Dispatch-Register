7ca97e75da44c48091324fdd16d28fae95af69bb
Abdullah <95099545+abdullah-9211@users.noreply.github.com>
2023-05-04 13:03:59 +0500

    Navigation between main page and related pages added

--- src/main/java/com/example/cheez/documentdispatch/HistoryController.java
+++ src/main/java/com/example/cheez/documentdispatch/HistoryController.java
@@ -4,14 +4,24 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 public class HistoryController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
 
     @FXML
     public void initialize() {
@@ -49,7 +59,15 @@ public class HistoryController {
 
     @FXML
     void goBack(ActionEvent event) {
-
+        try {
+            root = FXMLLoader.load(getClass().getResource("main_page.fxml"));
+            stage = (Stage) backbtn.getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
--- src/main/java/com/example/cheez/documentdispatch/Signup.java
+++ src/main/java/com/example/cheez/documentdispatch/Signup.java
@@ -2,10 +2,21 @@ package com.example.cheez.documentdispatch;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Signup {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
 
     @FXML
     private Button addUser;
@@ -27,7 +38,15 @@ public class Signup {
 
     @FXML
     void addUser(ActionEvent event) {
-
+        try {
+            root = FXMLLoader.load(getClass().getResource("main_page.fxml"));
+            stage = (Stage) addUser.getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
