501981f7090782d44a51db9b9de7c86e386a15b3
Abdullah <95099545+abdullah-9211@users.noreply.github.com>
2023-05-08 01:10:14 +0500

    Tracking Page made dynamic

--- src/main/java/com/example/cheez/documentdispatch/TrackingPageController.java
+++ src/main/java/com/example/cheez/documentdispatch/TrackingPageController.java
@@ -1,16 +1,21 @@
 package com.example.cheez.documentdispatch;
 
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class TrackingPageController {
+public class TrackingPageController implements Initializable {
 
     private Stage stage;
     private Scene scene;
@@ -18,6 +23,22 @@ public class TrackingPageController {
 
     @FXML
     private Button done_btn;
+    @FXML
+    private Text firstID;
+    @FXML
+    private Text secondID;
+    @FXML
+    private Text thirdID;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        DocumentHolder docHolder = DocumentHolder.getInstance();
+        List<String> routes = docHolder.document.route;
+        firstID.setText(" " + routes.get(0));
+        secondID.setText(" " + routes.get(1));
+        thirdID.setText(" " + routes.get(2));
+    }
+
 
     @FXML
     void donetracking(ActionEvent event) {
